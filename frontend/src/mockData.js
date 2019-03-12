c319b69c9a70deb5dbcc9ee8edcafde7939b69a9
Neil Mills <accounts@digital-one.co.uk>
2019-03-12 01:45:58 +0000

    Add testing packages to build

---
+++ frontend/src/mockData.js
@@ -0,0 +1,182 @@
+export const outcomes = [
+    {
+        outcomeId: 367530493,
+        marketId: 93649849,
+        eventId: 21249939,
+        name: "Yes",
+        displayOrder: 10,
+        place: 0,
+        result: "-",
+        favourite: false,
+        decimal: "2.25",
+        num: "5",
+        den: "4",
+        active: true,
+        resulted: false,
+        cashoutable: false,
+        displayable: true,
+        suspended: false,
+        price: {
+            decimal: "34",
+            num: "33",
+            den: "1",
+        },
+        status: {
+            displayable: true
+        }
+    },
+    {
+        outcomeId: 367530494,
+        marketId: 93649849,
+        eventId: 21249939,
+        name: "No",
+        displayOrder: 10,
+        place: 0,
+        result: "-",
+        favourite: false,
+        decimal: "2.25",
+        num: "5",
+        den: "4",
+        active: true,
+        resulted: false,
+        cashoutable: false,
+        displayable: true,
+        suspended: false,
+        price: {
+            decimal: "34",
+            num: "33",
+            den: "1",
+        },
+        status: {
+            displayable: true
+        }
+    }
+];
+
+export const markets = [
+    {
+        marketId: 93649849,
+        eventId: 21249939,
+        name: "Both Score No Draw",
+        displayOrder: -32496,
+        type: "standard",
+        spAvail: false,
+        status: {
+            displayable: true
+        },
+        outcomes: [367530493, 367530501]
+    },
+    {
+        marketId: 93649850,
+        eventId: 21249939,
+        name: "Both Score No Draw",
+        displayOrder: -32496,
+        type: "standard",
+        spAvail: false,
+        status: {
+            displayable: true
+        },
+        outcomes: [367530493, 367530501]
+    },
+]
+export const liveEventsData = [
+    {
+        eventId: 21249939,
+        name: "Shanghai Shenhua 0 v 0 Shandong Luneng Taishan",
+        displayOrder: 5,
+        sort: "MTCH",
+        linkedEventId: 21228740,
+        classId: 5,
+        className: "Football",
+        typeId: 10003971,
+        typeName: "Football Live",
+        linkedEventTypeId: 10005942,
+        linkedEventTypeName: "Chinese Super League",
+        startTime: "2017-09-19T11:35:23.000Z",
+        boostCount: 0,
+        superBoostCount: 0,
+        scores: {
+            home: 0,
+            away: 0
+        },
+        competitors: [{
+            name: "Shanghai Shenhua",
+            position: "home"
+        },
+        {
+            name: "Shandong Luneng Taishan",
+            position: "away"
+        }],
+        status: {
+            displayable: true
+        },
+        markets: [93649849],
+    },
+    {
+        eventId: 21249940,
+        name: "Shanghai Shenhua 0 v 0 Shandong Luneng Taishan",
+        displayOrder: 10,
+        sort: "MTCH",
+        linkedEventId: 21228740,
+        classId: 5,
+        className: "Football",
+        typeId: 10003971,
+        typeName: "Football Live",
+        linkedEventTypeId: 10005942,
+        linkedEventTypeName: "Chinese Super League",
+        startTime: "2017-09-19T11:35:23.000Z",
+        boostCount: 0,
+        superBoostCount: 0,
+        scores: {
+            home: 0,
+            away: 0
+        },
+        competitors: [{
+            name: "Shanghai Shenhua",
+            position: "home"
+        },
+        {
+            name: "Shandong Luneng Taishan",
+            position: "away"
+        }],
+        status: {
+            displayable: true
+        },
+        markets: [93649849],
+    },
+];
+
+export const eventData = [
+    {
+        eventId: 21249939,
+        name: "Shanghai Shenhua 0 v 0 Shandong Luneng Taishan",
+        displayOrder: 5,
+        sort: "MTCH",
+        linkedEventId: 21228740,
+        classId: 5,
+        className: "Football",
+        typeId: 10003971,
+        typeName: "Football Live",
+        linkedEventTypeId: 10005942,
+        linkedEventTypeName: "Chinese Super League",
+        startTime: "2017-09-19T11:35:23.000Z",
+        boostCount: 0,
+        superBoostCount: 0,
+        scores: {
+            home: 0,
+            away: 0
+        },
+        competitors: [{
+            name: "Shanghai Shenhua",
+            position: "home"
+        },
+        {
+            name: "Shandong Luneng Taishan",
+            position: "away"
+        }],
+        status: {
+            displayable: true
+        },
+        markets: [93649849, 93649850],
+    },
+];
